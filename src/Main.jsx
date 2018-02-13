3c13350971c90203ce6632078949eca3b87cf5bd
Rony Francis <ronydfrnd@gmail.com>
2018-02-13 16:12:20 +0530

    react-router added

---
+++ src/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+
+/**
+ * [App says hi]
+ */
+const Main = () => <div>main</div>;
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import Main from './Main';
 import './index.css';
 
 // import App from './App';
 import Movies from './components/Movies';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Movies name='rony'  />,
- document.getElementById('root'));
+// ReactDOM.render(
+//   <Movies name='rony'  />,
+//  document.getElementById('root'));
+
+// class App extends Component {
+//   render() {
+//     return (
+//
+//      );
+//    }
+//  }
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={Main} />
+        <Route path="/movies" render={() => <Movies name="rony" />} />
+      </div>
+    </Router>
+  );
+};
+
+// ReactDOM.render(<App/>, document.getElementById('root'));
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
 registerServiceWorker();
